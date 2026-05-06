76a57141d90f8781a5fd0e9ec9886ec74bd6ca71
AsmaLR <asmalahmar200124@gmail.com>
2026-05-06 01:02:46 +0100

    Create calculatrice.py

---
+++ calculatrice.py
@@ -0,0 +1,14 @@
+# Simple Calculator
+print("--- My Calculator ---")
+# The program asks for numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# The equations
+plus = num1 + num2
+minus = num1 - num2
+multiply = num1 * num2
+# Showing the results
+print("Total Addition: ", plus)
+print("Total Subtraction: ", minus)
+print("Total Multiplication: ", multiply)
+print("Done.")
